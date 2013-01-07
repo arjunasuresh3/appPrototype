97cb4afc09c64f316ef41f9f63a856f7a39db4d6
Arjuna <arjunasuresh3@gmail.com>
2013-01-07 20:17:41 +0530

    removed tryouts

--- initialtryouts/zecalendar.js
+++
@@ -1,3 +0,0 @@
-YUI.add('zecalendar', function (Y, NAME) {
-    Y.ZeCalendar = Y.Base.create('calendar', Y.Calendar, [Y.WidgetPosition, Y.WidgetPositionAlign, Y.WidgetPositionConstrain])
-}, '1.0.0', {"requires": ["calendar", "widget-position", "widget-position-align", "widget-position-constrain"]})
